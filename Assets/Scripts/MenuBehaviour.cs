ac567aff9943fa04e540325c0b8b51d6e9f28845
Jackson Lo <7jackson.lo@gmail.com>
2016-05-16 21:24:14 -0400

    script for start button on menu

---
+++ Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBehaviour : MonoBehaviour {
+
+	bool startActivated = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startActivated) {
+			//start game and change game state to game mode
+		}
+	}
+
+	void onMouseDown() {
+		startActivated = true;
+	}
+}
