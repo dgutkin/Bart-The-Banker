86f927d338b4e6fde5fed97d25dcb059141bc456
dgutkin <dgutkin6@yahoo.ca>
2016-05-17 22:39:52 -0400

    game start transition

---
+++ Assets/Scripts/GameStates.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameStates : MonoBehaviour {
+
+	public static bool gameActive = true;
+
+	public enum displayStates {
+
+		menuScreen = 0,
+		gameScreen
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void changeDisplayState(displayStates newState) {
+
+		Debug.Log("change");
+		switch (newState) {
+		case displayStates.menuScreen:
+			gameActive = false;
+			SceneManager.LoadScene (0);
+			break;
+		case displayStates.gameScreen:
+			gameActive = true;
+			SceneManager.LoadScene (1);
+			break;
+		}
+
+	}
+
+	public void startGame() {
+		Debug.Log("start");
+		changeDisplayState (displayStates.gameScreen);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/MenuBehaviour.cs
+++ Assets/Scripts/MenuBehaviour.cs
@@ -4,20 +4,24 @@ using System.Collections;
 public class MenuBehaviour : MonoBehaviour {
 
 	bool startActivated = false;
+	public GameStates stateManager = null;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (startActivated) {
+			Debug.Log("activated");
 			//start game and change game state to game mode
+			stateManager.startGame();
 		}
 	}
 
-	void onMouseDown() {
+	void OnMouseDown() {
+		Debug.Log("mouse_down");
 		startActivated = true;
 	}
 }
