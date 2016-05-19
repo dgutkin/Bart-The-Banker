774d649b1569fccbde2e5833092325946b1989cc
dgutkin <dgutkin6@yahoo.ca>
2016-05-18 21:05:22 -0400

    camera fix and tiling for background/midground/ground
    camera only follows player horizontally instead of jumping with the
    player

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		
+		this.gameObject.transform.position = new Vector3(player.position.x, this.gameObject.transform.position.y,this.gameObject.transform.position.z);
+
+	}
+}
---
+++ Assets/Scripts/Tiling.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+ [RequireComponent (typeof(SpriteRenderer))]
+
+public class Tiling : MonoBehaviour { // attach as component to foreground
+
+	public int offsetX = 2;
+
+	public bool hasRight = false;
+	public bool hasLeft = false;
+
+	public bool reverseScale = false; // if object not tilale
+
+	private float spriteWidth = 2f;
+	private Camera cam;
+	private Transform myTransform;
+
+	void Awake() {
+		cam = Camera.main;
+		myTransform = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer sRenderer = GetComponent<SpriteRenderer> ();
+		spriteWidth = sRenderer.sprite.bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hasLeft == false || hasRight == false) {
+			float camHorizontalExtend = cam.orthographicSize * Screen.width / Screen.height;
+			float edgeVisiblePositionRight = (myTransform.position.x + spriteWidth / 2) - camHorizontalExtend;
+			float edgeVisiblePositionLeft = (myTransform.position.y - spriteWidth / 2) + camHorizontalExtend;
+
+			if (cam.transform.position.x >= edgeVisiblePositionRight - offsetX && hasRight == false) {
+				makeNewGround (1);
+				hasRight = true;
+			} else if (cam.transform.position.x <= edgeVisiblePositionLeft - offsetX && hasLeft == false) {
+				makeNewGround (1);
+				hasLeft = true;
+			}
+		}
+	}
+
+	void makeNewGround (int direction) {
+		Vector3 newPosition = new Vector3 (myTransform.position.x + spriteWidth * direction, myTransform.position.y);
+		Transform newGround = Instantiate (myTransform, newPosition, myTransform.rotation) as Transform;
+
+		if (reverseScale == true) {  // if not tileable reverse x scale
+			newGround.localScale = new Vector3 (newGround.localScale.x * -1, newGround.localScale.y, newGround.localScale.z);
+		}
+
+		newGround.parent = myTransform.parent;
+		if (direction > 0) {
+			newGround.GetComponent<Tiling> ().hasLeft = true;
+		} else {
+			newGround.GetComponent<Tiling> ().hasRight = true;
+		}
+	}
+}
