980320b4a64e2a90787ddd0385d7e41d38dcb063
Jackson Lo <7jackson.lo@gmail.com>
2016-06-23 21:53:36 -0400

    New gameitem generation

--- Assets/Scripts/GameItemGenerator.cs
+++ Assets/Scripts/GameItemGenerator.cs
@@ -21,14 +21,14 @@ public class GameItemGenerator : MonoBehaviour {
 		
 	void SpawnObjects() {
 		//Gavel Position
-		for (int i = 0; i < 20; i++) {
-			int randomX = Random.Range (40, 70);
-			int randomY = Random.Range (1, 3);
+		for(int i = 0; i < 20; ++i) {
+			int randomX = Random.Range (50, 100);
+			int randomY = Random.Range (-43, 0);
 
-			if (randomY == 1) {
-				randomY = 15;
-			} else {
-				randomY = 30;
+			if (randomY > -40 && randomY < -10) {
+				Vector2 billPosition = originPosition + new Vector2 (randomX/10, randomY/10);
+				Instantiate (bill, billPosition, Quaternion.identity);
+				continue;
 			}
 
 			Vector2 randomPosition = originPosition + new Vector2 (randomX/10, randomY/10);
@@ -39,11 +39,6 @@ public class GameItemGenerator : MonoBehaviour {
 			originPosition.x = randomPosition.x;
 
 		}
-
-		/*for (int i = 0; i < billSpawnPoints.Length; i++) {
-			int randomX = Random.Range (2, 5);
-			int randomY = Random.Range (2, 5);
-			Instantiate (gavel, billSpawnPoints [i].position, Quaternion.identity);
-		}*/
 	}
+		
 }
