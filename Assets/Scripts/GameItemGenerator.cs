d67843e55f130a17ba86b916c1e4ffbed4720c33
Jackson Lo <7jackson.lo@gmail.com>
2016-06-25 02:50:05 -0400

    Box spawn generation algorithm

--- Assets/Scripts/GameItemGenerator.cs
+++ Assets/Scripts/GameItemGenerator.cs
@@ -5,12 +5,14 @@ public class GameItemGenerator : MonoBehaviour {
 
 	public GameObject gavel;
 	public GameObject bill;
+	public GameObject box;
 
 	private Vector2 originPosition;
 
 	// Use this for initialization
 	void Start () {
 		originPosition = transform.position;
+		originPosition += new Vector2 (5f, -3.72f);
 		SpawnObjects ();
 	}
 	
@@ -20,25 +22,114 @@ public class GameItemGenerator : MonoBehaviour {
 	}
 		
 	void SpawnObjects() {
-		//Gavel Position
-		for(int i = 0; i < 20; ++i) {
-			int randomX = Random.Range (50, 100);
-			int randomY = Random.Range (-43, 0);
+		//Origin is Vector2 (2, 0), Floor is (7, -4.92)
+		int blocksGenerated = 30;
+		int[] blockMap = new int[30];
 
-			if (randomY > -40 && randomY < -10) {
-				Vector2 billPosition = originPosition + new Vector2 (randomX/10, randomY/10);
-				Instantiate (bill, billPosition, Quaternion.identity);
+		//Box Positions
+		//Random first block
+		int firstRandom = Random.Range(0, 100);
+		float blockHeight = 1.2f;
+		float blockWidth = 1.2f;
+		Vector2 prevBlockPos = originPosition;
+		int prevBlockType;
+
+		float blockType1 = originPosition.y;
+		float blockType2 = originPosition.y + 3 * blockHeight;
+		float blockType3 = originPosition.y + 4 * blockHeight;
+
+		if (firstRandom < 70) {
+			//Floor block (1)
+			prevBlockPos.y = blockType1;
+			prevBlockType = 1;
+		} else if (firstRandom < 75) {
+			//Head block (2)
+			prevBlockPos.y = blockType2;
+			prevBlockType = 2;
+		} else {
+			//Jump block (3)
+			prevBlockPos.y = blockType3;
+			prevBlockType = 3;
+		}
+		Instantiate (box, prevBlockPos, Quaternion.identity);
+		Debug.Log (prevBlockPos.ToString());
+
+		System.Random random = new System.Random ();
+		int blocksMade = 0;
+		int noBlock = 0;
+
+		for(int i = 0; i < blocksGenerated; ++i) {
+			int noBlockP = Random.Range (0, 100);
+
+			//No Block
+			if (noBlockP < 20 - noBlock * 5) {
+				noBlock++;
+				blockMap [i] = 0;
 				continue;
 			}
 
-			Vector2 randomPosition = originPosition + new Vector2 (randomX/10, randomY/10);
-			Debug.Log (randomY);
+			//Make a block
+			int rnd = random.Next(0, 100);
+			switch (prevBlockType) {
+			case 1:
+				if (rnd < 33) {
+					prevBlockPos += new Vector2 (Mathf.Max(5 * blockWidth, noBlock * blockWidth), 0);
+					prevBlockType = 1;
+				} else if (rnd < 66) {
+					prevBlockPos += new Vector2 (Mathf.Max(4 * blockWidth, noBlock * blockWidth), 0);
+					prevBlockType = 2;
+				} else {
+					prevBlockPos += new Vector2 (Mathf.Max (4 * blockWidth, noBlock * blockWidth), 0);
+					prevBlockType = 3;
+				}
+				break;
+			case 2:
+				if (rnd < 40) {
+					prevBlockPos += new Vector2 (Mathf.Max(3 * blockWidth, noBlock * blockWidth), 0);
+					prevBlockType = 1;
+				} else if (rnd < 60) {
+					prevBlockPos += new Vector2 (Mathf.Max(1 * blockWidth, noBlock * blockWidth), 0);
+					prevBlockType = 2;
+				} else {
+					prevBlockPos += new Vector2 (Mathf.Max (1 * blockWidth, noBlock * blockWidth), 0);
+					prevBlockType = 3;
+				}
+				break;
+			case 3:
+				if (rnd < 60) {
+					prevBlockPos += new Vector2 (Mathf.Max(2 * blockWidth, noBlock * blockWidth), 0);
+					prevBlockType = 1;
+				} else if (rnd < 80) {
+					prevBlockPos += new Vector2 (Mathf.Max(1 * blockWidth, noBlock * blockWidth), 0);
+					prevBlockType = 2;
+				} else {
+					prevBlockPos += new Vector2 (Mathf.Max (1 * blockWidth, noBlock * blockWidth), 0);
+					prevBlockType = 3;
+				}
+				break;
+			}
 
-			Instantiate (gavel, randomPosition, Quaternion.identity);
+			switch (prevBlockType) {
+			case 1:
+				prevBlockPos.y = blockType1;
+				break;
+			case 2:
+				prevBlockPos.y = blockType2;
+				break;
+			case 3: 
+				prevBlockPos.y = blockType3;
+				break;
+			}
 
-			originPosition.x = randomPosition.x;
-
+			Instantiate (box, prevBlockPos, Quaternion.identity);
+			blocksMade++;
+			noBlock = 0;
+			blockMap [i] = prevBlockType;
+			Debug.Log (i + ": BlockType" + prevBlockType + " Made At: " + prevBlockPos);
 		}
+
+		//Generate Bills
+
 	}
 		
 }
