ad480f100d98cd54cf5cb802aae11355ab64b027
dgutkin <dngutkin@gmail.com>
2017-05-21 23:48:17 -0400

    clearer message on what to avoid when starting the game

--- Assets/Scripts/InstructionOverlayBehaviour.cs
+++ Assets/Scripts/InstructionOverlayBehaviour.cs
@@ -7,10 +7,18 @@ public class InstructionOverlayBehaviour : MonoBehaviour {
 
 	public GameObject rightSideInstructions;
 	public GameObject leftSideInstructions;
+	public GameObject avoidInstructions;
+	public GameObject avoidCage;
+	public GameObject avoidCop;
+	public GameObject avoidTax;
 
 	private bool _showOverlay = false;
 	private Text _rightSide;
 	private Text _leftSide;
+	private Text _avoidText;
+	private SpriteRenderer _avoidCageRenderer;
+	private SpriteRenderer _avoidCopRenderer;
+	private SpriteRenderer _avoidTaxRenderer;
 
 	// Use this for initialization
 	void Start () {
@@ -19,19 +27,35 @@ public class InstructionOverlayBehaviour : MonoBehaviour {
 		_rightSide.enabled = false;
 		_leftSide = leftSideInstructions.GetComponent<Text> ();
 		_leftSide.enabled = false;
+		_avoidText = avoidInstructions.GetComponent<Text> ();
+		_avoidText.enabled = false;
+		_avoidCageRenderer = avoidCage.GetComponent<SpriteRenderer> ();
+		_avoidCageRenderer.enabled = false;
+		_avoidCopRenderer = avoidCop.GetComponent<SpriteRenderer> ();
+		_avoidCageRenderer.enabled = false;
+		_avoidTaxRenderer = avoidTax.GetComponent<SpriteRenderer> ();
+		_avoidTaxRenderer.enabled = false;
 
 		if (PlayerPrefs.HasKey ("leaderboards")) {
 			// Only show the overlay with instructions if less than 4 high scores recorded
 			List<string> leaderboards = new List<string> (PlayerPrefs.GetString ("leaderboards").Split (';'));
-			if (leaderboards.Count < 4) {
+			if (leaderboards.Count < 400) {
 				_showOverlay = true;
 				_rightSide.enabled = true;
 				_leftSide.enabled = true;
+				_avoidText.enabled = true;
+				_avoidCageRenderer.enabled = true;
+				_avoidCopRenderer.enabled = true;
+				_avoidTaxRenderer.enabled = true;
 			}
 		} else {
 			_showOverlay = true;
 			_rightSide.enabled = true;
 			_leftSide.enabled = true;
+			_avoidText.enabled = true;
+			_avoidCageRenderer.enabled = true;
+			_avoidCopRenderer.enabled = true;
+			_avoidTaxRenderer.enabled = true;
 		}
 
 	}
@@ -45,6 +69,10 @@ public class InstructionOverlayBehaviour : MonoBehaviour {
 			float fadeDuration = 0.5f;
 			StartCoroutine(Utility.FadeTextOut(_rightSide, showDuration, fadeDuration));
 			StartCoroutine(Utility.FadeTextOut(_leftSide, showDuration, fadeDuration));
+			StartCoroutine (Utility.FadeTextOut (_avoidText, showDuration, fadeDuration));
+			StartCoroutine (Utility.FadeSpriteOut (_avoidCageRenderer, showDuration, fadeDuration));
+			StartCoroutine (Utility.FadeSpriteOut (_avoidCopRenderer, showDuration, fadeDuration));
+			StartCoroutine (Utility.FadeSpriteOut (_avoidTaxRenderer, showDuration, fadeDuration));
 			_showOverlay = false;
 
 		}
--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -6,10 +6,19 @@ using UnityEngine.UI;
 public static class Utility {
 
 	public static IEnumerator FadeTextOut(Text text, float showDuration, float fadeDuration) {
+		
 		yield return new WaitForSeconds (showDuration);
 		text.CrossFadeAlpha (0f, fadeDuration, false);
 		yield return new WaitForSeconds (fadeDuration);
 		text.enabled = false;
+
+	}
+
+	public static IEnumerator FadeSpriteOut(SpriteRenderer renderer, float showDuration, float fadeDuration) {
+		
+		yield return new WaitForSeconds (showDuration);
+		renderer.enabled = false;
+
 	}
 		
 }
